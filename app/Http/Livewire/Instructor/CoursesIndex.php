2f77622e42d9aafa13418e93a4932ae8fdc15b8d
Marcus Moreira de Souza <marcus@idevs.com.br>
2021-06-04 18:21:24 -0300

    refactory

---
+++ app/Http/Livewire/Instructor/CoursesIndex.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire\Instructor;
+
+use Livewire\Component;
+use App\Models\Course;
+use Livewire\WithPagination;
+
+class CoursesIndex extends Component
+{
+    use WithPagination;
+    public $search;
+
+    public function render()
+    {
+        //$courses = Course::where('user_id', auth()->user()->id)->get();
+        $courses = Course::where('user_id', auth()->user()->id)
+                    ->where('title', 'LIKE', '%'.$this->search.'%')
+                    ->paginate(8);
+
+        return view('livewire.instructor.courses-index', compact('courses'));
+    }
+
+    public function resetPage() {
+        $this->reset('page');
+    }
+}
--- routes/instructor.php
+++ routes/instructor.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\InstructorCourses;
+use App\Http\Controllers\Instructor\CourseController;
 
 Route::redirect('', 'instructor/courses');
-Route::get('courses', InstructorCourses::class)->middleware('can:Cursos - Ler')->name('courses.index');
+
+//Route::get('courses', InstructorCourses::class)->middleware('can:Cursos - Ler')->name('courses.index');
+
+Route::resource('courses', CourseController::class)->names('courses');
